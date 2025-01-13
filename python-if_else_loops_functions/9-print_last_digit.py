62848e041120244ef30d500b0c014e5d47cda93e
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-13 18:05:32 +0100

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,8 @@
 
 def print_last_digit(number):
     if number < 0:
-        return (-number) % 10
+        last_digit = (-number) % 10
     else:
-        return number % 10
+        last_digit = number % 10
+
+    print("{}".format(last_digit), end="")
