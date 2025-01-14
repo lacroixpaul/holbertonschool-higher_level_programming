6ef40c7e144a225448862c9e7479fec4092198f2
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-14 10:19:53 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) == 1:
-    print("{:d} arguments.".format(len(argv) - 1))
-elif len(argv) == 2:
-    print("{:d} argument:".format(len(argv) - 1))
-    print("{:d}: {}".format(len(argv) - 1, argv[1]))
-else:
-    print("{:d} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+        print("{:d}: {}".format(len(argv) - 1, argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
