49c0056ed708732d49cf3a1841b6879bed5c5804
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-14 09:54:59 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if len(sys.argv) == 1:
     print("{:d} arguments.".format(len(sys.argv) - 1))
 elif len(sys.argv) == 2:
     print("{:d} argument:".format(len(sys.argv) - 1))
-    print("{:d} {}".format(len(sys.argv) - 1, sys.argv[1]))
+    print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
 else:
     print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
