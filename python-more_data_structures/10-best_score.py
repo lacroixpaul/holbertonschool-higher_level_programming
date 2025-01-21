dd86b433e20d042880a7b17b78a7e2ee793a41f1
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-21 09:46:20 +0100

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,5 @@
 def best_score(a_dictionary):
     if len(a_dictionary) == 0:
         return None
-    max_number = a_dictionary[0]
-    for i in range(len(a_dictionary)):
-        if a_dictionary[i] > max_number:
-            max_number = a_dictionary[i]
+    max_number = max(a_dictionary, key=a_dictionary.get)
     return max_number
