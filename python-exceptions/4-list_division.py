fc23357c92280870ef004924a0a02663075a269b
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-21 16:35:27 +0100

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             new_list.append(0)
         finally:
-            print("{}".format(new_list))
+            pass
     return new_list
