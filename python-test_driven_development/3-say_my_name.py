14fb68d36ba889f51fd8f6c0ad9ea5c66d854991
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-23 13:24:07 +0100

    3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 3-say_my_name
+Provides a function `say_my_name` that prints two names.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints My name is <first name> <last name>.
+    Args:
+        first_name (string): first name to print.
+        last_name (string): last name to print.
+    Raises:
+        TypeError: in case of non string type argument.
+
+    Returns:
+    None.
+
+    Example :
+    >>> say_my_name(John, Wick)
+    My name is John Wick
+
+    """
+    if not isinstance(first_name, str) or len(first_name) == 0:
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str) or len(last_name) == 0:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
