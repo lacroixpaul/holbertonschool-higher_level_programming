1225444aec514247c79a784404be361b962120c9
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-23 13:40:56 +0100

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -20,7 +20,7 @@ def print_square(size):
         ValueError: If size is less than 0.
     """
 
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
