64313a8c6a66a103be9cbe894ff3c13e512a127b
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-23 13:26:16 +0100

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -24,6 +24,6 @@ def say_my_name(first_name, last_name=""):
     """
     if not isinstance(first_name, str) or len(first_name) == 0:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str) or len(last_name) == 0:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
