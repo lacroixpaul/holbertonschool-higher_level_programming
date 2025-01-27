1b6083b73f0e706dcaba425348cc3ba16255bfc9
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 15:44:41 +0100

    9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -87,6 +87,7 @@ class Rectangle:
             return rect_1
         return rect_2
 
+    @classmethod
     def square(cls, size=0):
         """  returns a new Rectangle instance with width == height == size """
         if not isinstance(size, int):
