6a87db6e9ca70d6a8a15f407da6ef4103c129913
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 09:34:32 +0100

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 0-square.py
+Provides an empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Provides an empty class Square that defines a square
+
+    Attribute :
+        Size (private) of the square
+    """
+
+    def __init__(self, size=0):
+        if size is not type(int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
