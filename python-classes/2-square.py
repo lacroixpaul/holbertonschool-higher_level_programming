6a56ca1cd9d8addab9ee857150b0e64467010ed1
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 09:37:03 +0100

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,7 +14,7 @@ class Square:
     """
 
     def __init__(self, size=0):
-        if size is not type(int):
+        if size is not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
