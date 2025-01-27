c761f6f900e5fffb0bdb9e05cb9204b60ff59219
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 09:31:50 +0100

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 0-square.py
+Provides an empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Provides an empty class Square that defines a square
+
+    Attribute :
+        Size (private) of the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
