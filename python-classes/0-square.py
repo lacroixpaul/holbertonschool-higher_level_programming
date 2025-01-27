6f50e73715f0e6c2ae006ef414343608a470f4b4
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 09:23:03 +0100

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-square.py
+Provides an empty class Square that defines a square
+"""
+
+
+class square:
+    """
+    Provides an empty class Square that defines a square
+    """
+
+    pass
