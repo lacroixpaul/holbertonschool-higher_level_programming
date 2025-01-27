399ea7f27cc97b61b362b32c2783d2ecb24e40db
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-27 11:34:08 +0100

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Provides an empty class Square that defines a square
+"""
+
+
+class Rectangle:
+    """
+        Provides an empty class Rectangle that defines a rectangle
+    """
+    pass
