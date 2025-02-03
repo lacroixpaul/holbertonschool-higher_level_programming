b36e3f1869fdc51800ebdc839a14589dd41b209e
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 09:35:46 +0100

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup.py
+Provides a function that returns the list of attri. and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Provides a function that returns list of attri. and methods of an object
+    """
+
+    return dir(obj)
