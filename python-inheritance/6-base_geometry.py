309375ed38fc83fea35bc82686cab4aa35c59c57
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 10:52:54 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry.py
+Provides a class Square BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Provides a class Square BaseGeometry
+    """
+
+    def area(self):
+        """
+        Raises an Exception
+        """
+
+        raise Exception("area() is not implemented")
