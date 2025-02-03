5b8e72d1be4f09de27f3afd0ba23c33cfa839934
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 10:45:04 +0100

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 4-inherits_from.py
+Provides a function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Verify if the object is an instance of a class inherited from a other class
+    """
+    return isinstance(obj, a_class)
