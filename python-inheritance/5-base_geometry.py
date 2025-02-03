b92b8058d01916736ae4028ba8685af97d265127
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 10:50:03 +0100

    5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry.py
+Provides an empty class Square BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Provides an empty class Square BaseGeometry
+    """
+
+    pass
