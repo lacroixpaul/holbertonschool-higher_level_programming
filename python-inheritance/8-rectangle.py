d14330ceb4a373ed1f85315ade0eff68b8aac930
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 15:48:13 +0100

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
 """
-Module 7-base_geometry.py
-Provides a class Square BaseGeometry
+Module 8-base_geometry.py
+Provides a class Rectangle
 """
 
-
-class BaseGeometry:
-    """
-    Provides a class Square BaseGeometry
-    """
-
-    def area(self):
-        """
-        Raises an Exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates value
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
