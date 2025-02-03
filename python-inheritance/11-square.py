04ba80a37aa8cc1b5b12b9c81de4ca29ca17bc36
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 15:55:11 +0100

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 8-base_geometry.py
+Provides a class Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Child class Square of Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Initialize the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Define area of the square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+       Return the description of the square
+        """
+        return '[Square] {}/{}'.format(self.__size, self.__size)
