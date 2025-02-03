e9a7b2da740b6031d95d582285800a730835344e
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-03 10:41:30 +0100

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 3-is_kind_of_class.py
+Provides a function is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Verify if the object is an instance of the class or inherited class
+    """
+    return isinstance(obj) is a_class
