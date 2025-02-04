b5edc9220e936ca83c0733422b449b09c3d30e27
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-04 09:38:49 +0100

    task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+Module task_00_abc.py
+Provides an ABC class Animal with two subclasses
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    ABC class Animal
+    """
+
+
+@abstractmethod
+def sound(self):
+    """
+    sound ABC method
+    """
+    pass
+
+
+class Dog(Animal):
+    """
+    Dog class
+    """
+
+
+def sound(self):
+    """
+    Sound method
+    """
+    return ("Bark")
+
+
+class Cat(Animal):
+    """
+    Cat class
+    """
+
+
+def sound(self):
+    """
+    Sound method
+    """
+    return ("Meow")
