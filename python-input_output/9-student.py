c0c25da6fa26cec11c59b099f37e5dbd070adc16
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-10 11:19:23 +0100

    9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+9-student.py
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the square """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
