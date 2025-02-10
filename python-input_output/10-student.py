74cc0b03e832017c2db3e2efcd20bf9c64236fcd
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-10 12:04:22 +0100

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,10 +16,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         returns the dictionary description with simple data structure
         """
-        if isinstance(self, str):
-            return {"first_name": self.first_name, "last_name": self.last_name}
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if not isinstance(attr, str) or attr not in self.__dict__:
+                    return self.__dict__
+                result[attr] = self.__dict__[attr]
+            return result
         return self.__dict__
