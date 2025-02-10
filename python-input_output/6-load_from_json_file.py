c4d91e80ee06dc4658b0fe1427525cc9ff3dc6b5
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-10 10:00:22 +0100

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+6-load_from_json_file.py
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+    """
+    with open(filename, 'r') as f:
+        return json.load(f)
