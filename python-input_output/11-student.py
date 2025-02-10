6c09246ac753320697cabf169cfa10a457216777
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-10 12:22:10 +0100

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+11-student.py
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the square """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns the dictionary description with simple data structure
+        """
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for attr in attrs:
+            if isinstance(attr, str) and attr in self.__dict__:
+                result[attr] = self.__dict__[attr]
+        return result
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
