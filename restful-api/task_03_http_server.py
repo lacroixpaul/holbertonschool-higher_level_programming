6fd7dfdfb92c11e92f3cc5c62e651d23883a7d53
lacroixpaul <paul.lacroix892@outlook.fr>
2025-02-17 20:22:32 +0100

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,15 +12,10 @@ class mysubclass(BaseHTTPRequestHandler):
     subclass of http.server.BaseHTTPRequestHandler
     """
 
-
     def do_GET(self):
         """
         handle GET requests
         """
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
         if self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             json.dumps(data)
@@ -28,6 +23,20 @@ class mysubclass(BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+
 
 def run(server_class=HTTPServer, handler_class=mysubclass):
     """
