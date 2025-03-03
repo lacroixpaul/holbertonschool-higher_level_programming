2a9a0afc46af2ec330e359392e878e50389e1ba9
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-03 18:02:12 +0100

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 CREATE database IF NOT EXISTS hbtn_0d_usa
 use hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS states (
-	id INT primary key,
+	id INT AUTO_INCREMENT primary key,
 	name VARCHAR(256) NOT NULL
 )
