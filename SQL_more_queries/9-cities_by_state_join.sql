7037ffe27d0976a1bcbe93d5b282ddd440cfa8e7
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-04 09:54:14 +0100

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id = states.id
+INNER JOIN states
 ORDER BY cities.id ASC;
