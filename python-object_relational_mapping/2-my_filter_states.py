b253e4baa3cfc73b0573d03d769d5e8cf91201e0
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-05 11:37:08 +0100

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument."""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    cursor.execute(query, ('%' + state_name + '%',))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
