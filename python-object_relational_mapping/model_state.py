9969390342d14700fc070d12940f41971fc56e33
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-05 13:31:32 +0100

    model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  class definition of a State and
+an instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class State
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
